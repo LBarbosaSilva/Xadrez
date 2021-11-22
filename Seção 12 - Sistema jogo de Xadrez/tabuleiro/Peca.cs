43793e6da7cc3fa28e99d986ed64adfbbe4679e6
Luca Barbosa <lucasbarbosa1287@Gmail.com>
2021-11-22 17:59:04 -0300

    Last Commit

---
+++ Seção 12 - Sistema jogo de Xadrez/tabuleiro/Peca.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using tabuleiro;
+
+namespace tabuleiro
+{
+    abstract class Peca
+    {
+        public Posicao posicao { get; set; }
+        public Cor cor { get; protected set; }
+        public int qtd_Movimentos { get; protected set; }
+        public Tabuleiro tab { get; set; }
+
+        public Peca(Tabuleiro tab, Cor cor)
+        {
+            this.posicao = null;
+            this.tab = tab;
+            this.cor = cor;
+            this.qtd_Movimentos = 0;
+
+        }
+
+        public void incrementarQtdMovimentos()
+        {
+            qtd_Movimentos++;
+        }
+
+        public void decrementarQtdMovimentos()
+        {
+            qtd_Movimentos--;
+        }
+
+        public bool existeMovimentosPossiveis()
+        {
+            bool[,] mat = movimentosPossiveis();
+            for (int i = 0; i < tab.linhas; i++ )
+            {
+                for (int j = 0; j < tab.colunas; j++)
+                {
+                    if (mat[i, j])
+                    {
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+
+
+        public bool movimentoPossivel(Posicao pos)
+        {
+            return movimentosPossiveis()[pos.Linha, pos.Coluna];
+        }
+
+        public abstract bool[,] movimentosPossiveis();
+
+    }
+}
---
+++ Seção 12 - Sistema jogo de Xadrez/xadrez/Rei.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using tabuleiro;
+
+namespace xadrez
+{
+    class Rei : Peca
+    {
+
+        private PartidaDeXadrez partida;
+
+
+        public Rei(Tabuleiro tab, Cor cor, PartidaDeXadrez partida) : base(tab, cor)
+        {
+            this.partida = partida;
+        }
+
+        public override string ToString()
+        {
+            return "R";
+        }
+
+        private bool podeMover(Posicao pos)
+        {
+            Peca p = tab.peca(pos);
+            return p == null || p.cor != cor;
+        }
+
+        private bool testeTorreParaRoque(Posicao pos)
+        {
+            Peca p = tab.peca(pos);
+            return p != null && p is Torre && p.cor == cor && p.qtd_Movimentos == 0;
+        }
+
+        public override bool[,] movimentosPossiveis()
+        {
+            bool[,] mat = new bool[tab.linhas, tab.colunas];
+
+            Posicao pos = new Posicao(0, 0);
+
+            //acima
+            pos.definirValores(posicao.Linha - 1, posicao.Coluna);
+            if (tab.posicaoValida(pos) && podeMover(pos))
+            {
+                mat[pos.Linha, pos.Coluna] = true;
+            }
+
+            //ne
+            pos.definirValores(posicao.Linha - 1, posicao.Coluna + 1);
+            if (tab.posicaoValida(pos) && podeMover(pos))
+            {
+                mat[pos.Linha, pos.Coluna] = true;
+            }
+
+            //direita
+            pos.definirValores(posicao.Linha, posicao.Coluna + 1);
+            if (tab.posicaoValida(pos) && podeMover(pos))
+            {
+                mat[pos.Linha, pos.Coluna] = true;
+            }
+
+            //se
+            pos.definirValores(posicao.Linha + 1, posicao.Coluna + 1);
+            if (tab.posicaoValida(pos) && podeMover(pos))
+            {
+                mat[pos.Linha, pos.Coluna] = true;
+            }
+
+            //abaixo
+            pos.definirValores(posicao.Linha + 1, posicao.Coluna);
+            if (tab.posicaoValida(pos) && podeMover(pos))
+            {
+                mat[pos.Linha, pos.Coluna] = true;
+            }
+
+            //so
+            pos.definirValores(posicao.Linha + 1, posicao.Coluna - 1);
+            if (tab.posicaoValida(pos) && podeMover(pos))
+            {
+                mat[pos.Linha, pos.Coluna] = true;
+            }
+            //esquerda
+            pos.definirValores(posicao.Linha, posicao.Coluna - 1);
+            if (tab.posicaoValida(pos) && podeMover(pos))
+            {
+                mat[pos.Linha, pos.Coluna] = true;
+            }
+            //no
+            pos.definirValores(posicao.Linha - 1, posicao.Coluna - 1);
+            if (tab.posicaoValida(pos) && podeMover(pos))
+            {
+                mat[pos.Linha, pos.Coluna] = true;
+            }
+
+            // # Jogada Especial: ROQUE
+
+            if (qtd_Movimentos == 0 && !partida.xeque)
+            {
+                // # Jogada Especial ROQUE PEQUENO
+                Posicao Post1 = new Posicao(posicao.Linha, posicao.Coluna + 3);
+                if (testeTorreParaRoque(Post1))
+                {
+
+                    Posicao p1 = new Posicao(posicao.Linha, posicao.Coluna + 1);
+                    Posicao p2 = new Posicao(posicao.Linha, posicao.Coluna + 2);
+                    if (tab.peca(p1) == null && tab.peca(p2) == null)
+                    {
+                        mat[posicao.Linha, posicao.Coluna + 2] = true;
+                    }
+                }
+            }
+
+            // # Jogada Especial ROQUE GRANDE
+            Posicao Post2 = new Posicao(posicao.Linha, posicao.Coluna - 4);
+            if (testeTorreParaRoque(Post2))
+            {
+
+                Posicao p1 = new Posicao(posicao.Linha, posicao.Coluna - 1);
+                Posicao p2 = new Posicao(posicao.Linha, posicao.Coluna - 2);
+                Posicao p3 = new Posicao(posicao.Linha, posicao.Coluna - 3);
+                if (tab.peca(p1) == null && tab.peca(p2) == null && tab.peca(p3) == null)
+                {
+                    mat[posicao.Linha, posicao.Coluna - 2] = true;
+                }
+            }
+
+            return mat;
+        }
+    }
+}
